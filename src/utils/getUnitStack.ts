2ce17d4da9bf79f2c128bb03c2ffa1fe4ad9500f
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-08-24 20:06:40 -0400

    refactor(getUnitStack): removed a bunch of unneeded code

--- src/utils/getUnitStack.ts
+++ src/utils/getUnitStack.ts
@@ -1,31 +1,14 @@
-import { IUnitStack, IDamagePotential, createZeroDamagePotentialAndEfficiancy, StackId } from "@/types/combat";
+import { IUnitStack, StackId } from "@/types/combat";
 import { Unit } from "./Unit";
-import { IUnitType, UNIT_CLASSES  } from "@/types";
-import { getUnitData } from "./getUnitData";
 import { calculateDamageEfficiencyPotential } from "./calculateDamageEfficiencyPotential";
 import { calculateDamageDistribution } from "./calculateDamageDistribution";
 
 export function getUnitStack(units: Unit[], protection: number, homeDefence: Boolean, id: StackId): IUnitStack {
-  let totalDmgPotUnprocessed: IDamagePotential =
-    createZeroDamagePotentialAndEfficiancy(UNIT_CLASSES);
-
   let newUnitList: Unit[] = []
   
   for (const unit of units) {
     for (let i = 0; i < unit.quantity; i++)
       newUnitList.push(unit)
-  }
-
-  for (const unit of newUnitList) {
-    const data = getUnitData(unit.genericName, unit.mode) as IUnitType
-    const unitStats = data.doctrineVariants[unit.doctrine][unit.level - 1]
-
-    for (const key of UNIT_CLASSES) {
-      const combatStatisticsKey = `vs${key}` as const
-      
-      totalDmgPotUnprocessed[key].attack += unitStats.combatStatistics[combatStatisticsKey].attack
-      totalDmgPotUnprocessed[key].defense += unitStats.combatStatistics[combatStatisticsKey].defense || 0
-    }
   }
 
   const [dmgEfficiency, dmgPotential] = calculateDamageEfficiencyPotential(newUnitList)
