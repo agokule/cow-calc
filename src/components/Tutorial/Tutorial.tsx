ee77b4dfb68acf572427b6526b4e666b6fe2664c
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-08-29 19:59:06 -0400

    chore(Tutorial): added disclaimer about inaccurate data

--- src/components/Tutorial/Tutorial.tsx
+++ src/components/Tutorial/Tutorial.tsx
@@ -37,8 +37,16 @@ export default function Tutorial() {
           <li>Drag and drop them to the blank sections</li>
           <li>Adjust unit properties using the dropdowns</li>
           <li>Click "Connections" to set which units battle each other</li>
-          <li>Finally, press "Start Battle Calculation" to see if you will win!</li>
+          <li>Finally, press "Start Battle Calculation" and "Finish" to see if you will win!</li>
         </ol>
+        <h3>Note:</h3>
+        <p>
+          The unit data for the Axis, Comintern, and Pan-Asian doctrines are estimates,
+          and may have minor inaccuracies.
+        </p>
+        <p>You can check for these by clicking on a unit in the sidebar</p>
+        <p>If you notice something that's wrong, and you know how to code, feel free to make a <a href="https://github.com/agokule/cow-calc/pulls">pull request </a></p>
+        <p>If you notice any bugs, feel fre to report them <a href="https://github.com/agokule/cow-calc/issues">here</a></p>
         <button className="tutorial-button" onClick={handleClose}>
           Got it, don't show this again.
         </button>
