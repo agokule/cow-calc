ba6b552f02c22b8d7c43d1e6ae931e781f5a548a
Atharv Gokule <gokuleatharv06@gmail.com>
2026-06-15 22:05:34 -0400

    chore: updated packages and added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { UnitListsProvider } from "@/context/UnitListsContext";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="google-site-verification" content="WUSYAGml9ozuLxtQ4Avy0qtgvabeIJAc9B9HcSE24lM" />
+      <Analytics/>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
